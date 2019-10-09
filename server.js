bc1c78d767e70369d6a136b1c818bce175ba2124
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2019-10-09 21:56:58 +0100

    minor changes

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const sqlite3 = require('sqlite3').verbose();
 
 app.get('/', (req, res) => {
+  access_db();
+  // res.send(res);
   res.send('Hello from App Engine!');
 });
 
@@ -10,3 +13,23 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+function access_db() {
+
+  let db = new sqlite3.Database('./resources/database.db');
+  let res = null;
+  let sql = 'SELECT * FROM sqlite_sequence';
+
+  db.all(sql, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    rows.forEach((row) => {
+      res = row.name + " " + row.seq + "\n";
+      console.log(res);
+    });
+  });
+
+  db.close();
+  return res;
+};
