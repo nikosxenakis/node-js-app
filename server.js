efeb6e4eca8e7a7498e9ccc58e4a93b77ffbf16b
Nikolaos Xenakis <nikosxenakis94@gmail.com>
2019-10-09 22:46:33 +0100

    dsa

--- server.js
+++ server.js
@@ -10,9 +10,11 @@ app.get('/', (req, res) => {
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
+
+module.exports = server;
 
 function access_db() {
 
